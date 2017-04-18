8bb972bd9d0e41e5c98d9225ec08c0ef9a7f5374
poetlifeart <vramezani@gmail.com>
2017-04-18 07:32:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,26 +10,26 @@ using namespace std;
 int main()
 {
 	
-  int dim=16;  
+ int dim=16;  
  
    Tree< int > intTree(16); // create Tree of int values
  
   
  //  int  value[dim]={ 21, 122, 3, 1, 43, 311, 31, 14, 236, 12, 4321, 5, 888, 444, 777, 222, 999, 111, 333, 555, 666,
-  //  11122, 22211, 6677, 8899, 9988, 100000, 10101, 10293, 54023, 3213450, 4309004};
+ //  11122, 22211, 6677, 8899, 9988, 100000, 10101, 10293, 54023, 3213450, 4309004};
   
   
   
-  int  value[dim]={ 21, 122, 3, 1, 43, 311, 31, 14, 236, 12, 4321, 5, 888, 444, 777, 222};
+ int  value[dim]={ 21, 122, 3, 1, 43, 311, 31, 14, 236, 12, 4321, 5, 888, 444, 777, 222};
   
   
-   for ( int i = 0; i < dim; i++ ) 
+  for ( int i = 0; i < dim; i++ ) 
    {
      
       intTree.insertNode( value[i] );
    } // end for
 
-  intTree.dsw();
+     intTree.dsw();
 
    return 0;
 } // end main
